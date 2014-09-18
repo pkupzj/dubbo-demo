8eccd1e71d5376bbc22a7deb651c72e15278ba58
panzijian <zijian.pan@renren-inc.com>
2014-09-18 18:42:16 +0800

    update

--- dubbo-demo-consumer/src/main/java/com/zijian/dubbo/demo/consumer/Consumer.java
+++ dubbo-demo-consumer/src/main/java/com/zijian/dubbo/demo/consumer/Consumer.java
@@ -1,7 +1,6 @@
 package com.zijian.dubbo.demo.consumer;
 
 import com.zijian.dubbo.demo.serviceA.client.DemoServiceAClient;
-import com.zijian.dubbo.demo.serviceB.client.DemoServiceBClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -15,22 +14,22 @@ public class Consumer {
 	@Autowired
 	private DemoServiceAClient serviceAClient;
 
-	@Autowired
-	private DemoServiceBClient serviceBClient;
+//	@Autowired
+//	private DemoServiceBClient serviceBClient;
 
 	public void helloServiceA() {
 		serviceAClient.hello("consumer");
 	}
 
-	public void helloServiceB() {
-		serviceBClient.hello("consumer");
-	}
+//	public void helloServiceB() {
+//		serviceBClient.hello("consumer");
+//	}
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext-serviceA-client.xml","applicationContext-demo-consumer.xml");
 		Consumer consumer = context.getBean(Consumer.class);
 		consumer.helloServiceA();
-		consumer.helloServiceB();
+//		consumer.helloServiceB();
 	}
 
 }
